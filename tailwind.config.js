7ae91c58614ad546325a4a1ca2b5b6f9aa91b3e9
Andrea Gajelan <andrea.gajelan@tup.edu.ph>
2024-05-23 15:12:03 +0800

    Configured tailwind css on login page and implemented glass morphism design

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '.public/*.{html,js,php}', // Adjust the paths to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
